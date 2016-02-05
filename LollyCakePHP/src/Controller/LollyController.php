a7519d2a7dbd73faaca5cd608f75b81e6d90cbb9
zhao wei <zwvista@msn.com>
2016-02-05 19:20:20 +0900

    cakePHP MVC

--- LollyCakePHP/src/Controller/LollyController.php
+++ LollyCakePHP/src/Controller/LollyController.php
@@ -2,10 +2,37 @@
 
 namespace App\Controller;
 
+use Cake\ORM\TableRegistry;
+
 class LollyController extends AppController
 {
 	public function index()
 	{
-		
+		$langlist = TableRegistry::get('Languages')->find()->where('LANGID > 0')->all();
+		$this->set("langlist", $langlist);
+	}
+	
+	public function dictList()
+	{
+		$this->autoRender = false;
+		$langid = $this->request->data('selectedLangID');
+		$dictList = TableRegistry::get('Dictall')->find()->where('LANGID = ' . $langid)->all();
+		foreach($dictList as $dict) {
+	    	echo '<option>' . $dict['DICTNAME'] . '</option>';
+	    }
+	}
+	
+	public function dictUrl()
+	{
+		$this->autoRender = false;
+		$langid = $this->request->data('selectedLangID');
+		$dictname = $this->request->data('selectedDictName');
+		$word = $this->request->data('word');
+		$dictList = TableRegistry::get('Dictall')->find()
+			->where('LANGID = ' . $langid)
+			->andWhere("DICTNAME = '$dictname'")->all();
+		foreach($dictList as $dict) {
+	    	echo $dict['URL'];
+	    }
 	}
 }
